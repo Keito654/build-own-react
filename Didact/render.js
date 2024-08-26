1326757ffeee63426d00c3b89517db108a5233d7
Keito654 <38161062+Keito654@users.noreply.github.com>
2024-08-27 03:02:02 +0900

    a

--- Didact/render.js
+++ Didact/render.js
@@ -1,6 +1,6 @@
-export const render = (element, container) => {
+const createDom = (fiber) => {
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(element.type);
 
@@ -11,9 +11,61 @@ export const render = (element, container) => {
       dom[name] = element.props[name];
     });
 
-  element.props.children.forEach((child) => {
-    render(child, dom);
-  });
+  return dom;
+};
 
-  container.appendChild(dom);
+let nextUnitOfWork = null;
+
+export const render = (element, container) => {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+};
+
+const workLoop = (deadline) => {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+};
+
+requestIdleCallback(workLoop);
+
+const performUnitOfWork = (fiber) => {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
 };
